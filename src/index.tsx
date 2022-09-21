fb7ae118b999ca2dd5894c476ddfad9db249153b
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2022-09-21 16:30:45 +0100

    remove i18n

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import './i18n';
-import { featureFlag_getDebugRoom } from './features/data/utils/feature-flags';
-import { Logger } from './logger';
-
-const room = featureFlag_getDebugRoom();
-if (room) {
-  const logger = new Logger(room);
-  logger.start();
-}
+// import './i18n';
+// import { featureFlag_getDebugRoom } from './features/data/utils/feature-flags';
+// import { Logger } from './logger';
+//
+// const room = featureFlag_getDebugRoom();
+// if (room) {
+//   const logger = new Logger(room);
+//   logger.start();
+// }
 
 ReactDOM.render(<App />, document.getElementById('root'));
