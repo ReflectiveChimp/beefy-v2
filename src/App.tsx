b52476362f7d432989a65f5fd127bba8c3caacd6
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2022-09-21 17:02:01 +0100

    Revert "Remove helmet"
    This reverts commit 97e98e702b1723af81c188280b2f5f640c2f2335.

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,13 @@ import { store } from './store';
 import { featureFlag_replayReduxActions } from './features/data/utils/feature-flags';
 import { replayReduxActions } from './features/data/middlewares/debug/debug-replay';
 import { CowLoader } from './components/CowLoader';
+import { DefaultMeta } from './components/Meta';
+import { HelmetProvider } from 'react-helmet-async';
 
 const Home = React.lazy(() => import(`./features/home`));
+const Vault = React.lazy(() => import(`./features/vault`));
+const OnRamp = React.lazy(() => import(`./features/on-ramp`));
+const PageNotFound = React.lazy(() => import(`./features/pagenotfound`));
 
 export const App = () => {
   React.useEffect(() => {
@@ -32,12 +37,15 @@ export const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <WrappedFooter>
-        <Header />
-        <React.Suspense fallback={<CowLoader text="Loading" />}>
-          <Home />
-        </React.Suspense>
-      </WrappedFooter>
+      <HelmetProvider>
+        <DefaultMeta />
+        <WrappedFooter>
+          <Header />
+          <React.Suspense fallback={<CowLoader text="Loading" />}>
+            <Home />
+          </React.Suspense>
+        </WrappedFooter>
+      </HelmetProvider>
     </ThemeProvider>
   );
 };
---
+++ src/components/Meta/Meta.tsx
@@ -0,0 +1,32 @@
+import { memo, useMemo } from 'react';
+import { Helmet } from 'react-helmet-async';
+import { useTranslation } from 'react-i18next';
+
+export type MetaProps = {
+  title?: string;
+  description?: string;
+  noindex?: boolean;
+  disableTitleTemplate?: boolean;
+};
+export const Meta = memo<MetaProps>(function Meta({
+  title,
+  description,
+  noindex,
+  disableTitleTemplate = false,
+}) {
+  const { t } = useTranslation();
+  const fullTitle = useMemo(() => {
+    return title && !disableTitleTemplate ? t('Meta-Title-Template', { title }) : title;
+  }, [t, title, disableTitleTemplate]);
+
+  return (
+    <Helmet>
+      {fullTitle ? <title>{fullTitle}</title> : null}
+      {fullTitle ? <meta property="og:title" content={fullTitle} /> : null}
+      {fullTitle ? <meta property="twitter:title" content={fullTitle} /> : null}
+      {description ? <meta name="description" content={description} /> : null}
+      {description ? <meta property="og:description" content={description} /> : null}
+      {description ? <meta property="twitter:description" content={description} /> : null}
+    </Helmet>
+  );
+});
